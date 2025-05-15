b83fd0267c6c9a139f4d7be05a20acc1eb5ea474
Cyberfn <fernandononatocco@gmail.com>
2025-05-15 10:26:55 -0300

    Adiciona modais de sucesso e error

---
+++ modals.php
@@ -0,0 +1,29 @@
+<!-- Modal de Sucesso -->
+<div class="modal fade" id="modal_sucesso" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content border-success">
+      <div class="modal-header bg-success text-white">
+        <h5 class="modal-title" id="modal_sucesso_titulo">Sucesso</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body" id="modal_sucesso_mensagem">
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal de Erro -->
+<div class="modal fade" id="modal_erro" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content border-danger">
+      <div class="modal-header bg-danger text-white">
+        <h5 class="modal-title" id="modal_erro_titulo">Erro</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body" id="modal_erro_mensagem">
+        
+      </div>
+    </div>
+  </div>
+</div>
